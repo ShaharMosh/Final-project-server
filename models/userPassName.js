4082cef486e92252c91186e193d9fb20c070e557
danadanile <117977429+danadanile@users.noreply.github.com>
2024-01-10 16:55:00 +0200

    add user and userpass server side

---
+++ models/userPassName.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+// Define the UserPassName schema
+const UserPassNameSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+   
+    // contacts: [{
+    //     username:{
+    //     type: String
+    //     }
+    // }],
+
+});
+
+// Create a Mongoose model
+const UserPassName = mongoose.model('Users', UserPassNameSchema);
+
+// Connect to the MongoDB server
+mongoose.connect('mongodb://127.0.0.1:27017/DB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+export default UserPassName;
