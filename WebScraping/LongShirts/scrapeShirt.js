731f4158f36ed3ea40320c248b08a34ea5bd7a2f
Adi Ben Yehuda <adiby2017@gmail.com>
2024-01-10 17:50:03 +0200

    start the web scraping

---
+++ WebScraping/LongShirts/scrapeShirt.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import cheerio from "cheerio";
+
+// Function to perform the web scraping for a single website
+async function scrapeWebsite(url, config) {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+
+    const productInfo = [];
+
+    $(config.nameSelector).each((index, element) => {
+      const productName = $(element).find(config.nameSelector).text().trim();
+      //   const productPrice = $(element).find(config.priceSelector).text().trim();
+      //   const productImage = $(element).find(config.imageSelector).attr("src");
+      console.log(productName);
+      productInfo.push({
+        name: productName,
+        price: productPrice, // Uncomment this line if you have the priceSelector
+        image: productImage, // Uncomment this line if you have the imageSelector
+      });
+    });
+
+    console.log(`Scraped information from ${url}:`, productInfo);
+    return productInfo;
+  } catch (error) {
+    console.error(`Error scraping ${url}:`, error.message);
+    return [];
+  }
+}
+
+// Function to perform web scraping for multiple websites
+async function scrapeMultipleWebsites(websites) {
+  const scrapingPromises = websites.map(async (site) => {
+    return await scrapeWebsite(site.url, site.config);
+  });
+
+  try {
+    const results = await Promise.all(scrapingPromises);
+    // Process the results as needed
+    console.log("All scraping completed:", results);
+  } catch (error) {
+    console.error("Error scraping websites:", error);
+  }
+}
+
+export { scrapeWebsite, scrapeMultipleWebsites };
--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+import {
+  scrapeWebsite,
+  scrapeMultipleWebsites,
+} from "./WebScraping/LongShirts/scrapeShirt.js";
 
+// Example usage
+const websitesToScrape = [
+  {
+    url: "https://www.castro.com/women/categories/tops_/_bodysuits/long_shirts",
+    config: {
+      //   productSelector: ".product-class",
+      nameSelector: "#product_category_157204",
+      //   priceSelector: ".price-class",
+    },
+  },
+  //   {
+  //     url: "https://website2.com",
+  //     config: {
+  //       productSelector: ".item-class",
+  //       nameSelector: ".title-class",
+  //       priceSelector: ".cost-class",
+  //     },
+  //   },
+];
+
+scrapeMultipleWebsites(websitesToScrape);
