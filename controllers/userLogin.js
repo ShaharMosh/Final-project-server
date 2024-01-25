4e63127a32fffcdfda9e63ea8bbb76422b23b6f1
ShaharMosh <shaharmosho@gmail.com>
2024-01-25 11:01:52 +0200

    add hello user

--- controllers/userLogin.js
+++ controllers/userLogin.js
@@ -1,12 +1,13 @@
 import userLogin from "../services/userLogin.js";
 
 const login = async (req, res) => {
-  const token = await userLogin.isLogin(req.body.email, req.body.password);
+  const loginResult = await userLogin.isLogin(req.body.email, req.body.password);
 
-  if (token !== null) {
-    res.json({ token });
+  if (loginResult !== null) {
+    const { name, token } = loginResult;
+    res.json({ name, token });
   } else {
-    res.status(404).json({ error: " email or password" });
+    res.status(404).json({ error: "Invalid email or password" });
   }
 };
 
