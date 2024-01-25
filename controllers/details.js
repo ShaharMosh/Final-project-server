33bc9ccf071b3409f67687af21da959b8b83b0e9
ShaharMosh <shaharmosho@gmail.com>
2024-01-25 13:26:09 +0200

    fix bug in details

--- controllers/details.js
+++ controllers/details.js
@@ -1,17 +1,15 @@
 import detailsService from "../services/details.js";
 
 const updateUser = async (req, res) => {
-    console.log("controllers111")
   const exist = await detailsService.isExist(req.body.oldEmail);
+
   const error = detailsService.isValidDetails(
     req.body.email,
     req.body.firstName,
     req.body.lastName,
   );
-  console.log("controllers1")
   if (error === "" && !exist) {
-    console.log("controllers2")
-    const updateUser = await detailsService.updateUser(req, req.headers.authorization);
+    const updateUser = await detailsService.updateUser(req, req.body.oldEmail, req.headers.authorization);
     res.json(updateUser);
   } else if (exist) {
     res.status(409).json({ error: "Email already exists" });
