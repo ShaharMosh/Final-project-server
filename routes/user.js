5d4110120d80b775daa6127b1f7d96fb3a3e1106
danadanile <117977429+danadanile@users.noreply.github.com>
2024-05-18 10:10:50 +0300

    fix errors

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ import express from "express";
 var router = express.Router();
 import userController from "../controllers/user.js";
 
-router.route("/Users/:email").get((req, res) => {
+router.route("/Change").get((req, res) => {
   userController.getDetails(req, res);
 });
 
