c2224a385a1901edd5a9133aab1ef91ea9d14c1b
Adi Ben Yehuda <adiby2017@gmail.com>
2024-05-26 09:41:28 +0300

    color

--- services/itemDetails.js
+++ services/itemDetails.js
@@ -1,49 +1,47 @@
 import Item from "../models/item.js";
-import { getImages } from "../WebScraping/scraping.js";
+import { getImagesAndColors } from "../WebScraping/scraping.js";
 import { getConfig } from "../WebScraping/websitesHtml.js";
 import Store from "../models/store.js";
 import Size from "../models/size.js";
 import Color from "../models/color.js";
 
 const getItemDetails = async (id) => {
-    try {
-        const item = await Item.findById(id);
-        if (item) {
-            console.log("item-service:", item)
-            const store = await Store.findById(item.store);
-            const size = await Size.findById(item.size);
-            const color = await Color.findById(item.color);
-            const config = getConfig(store.name);
-            const url = item.url;
+  try {
+    const item = await Item.findById(id);
+    if (item) {
+      console.log("item-service:", item);
+      const store = await Store.findById(item.store);
+      const size = await Size.findById(item.size);
+      const color = await Color.findById(item.color);
+      const config = getConfig(store.name);
+      const url = item.url;
 
-            const img_col = await getImages(url, config);
-            console.log("img_col", img_col);
-            const images = img_col[0];
-            // const colors = img_col[1];
+      const img_col = await getImagesAndColors(url, config);
+      console.log("img_col", img_col);
+      const images = img_col[0];
+      const colors = img_col[1];
 
-            const itemDetails = {
-                id: item.id,
-                image: item.image,
-                price: item.price,
-                store: store.name,
-                name: item.name,
-                size: size.name,
-                color: color.name,
-                colors: item.color_url,
-                images: images
-            }
-            console.log("itemDetails", itemDetails)
-            return itemDetails;
-        }
-        return null;
-    } catch (err) {
-        console.error(err);
-        return null;
+      const itemDetails = {
+        id: item.id,
+        image: item.image,
+        price: item.price,
+        store: store.name,
+        name: item.name,
+        size: size.name,
+        color: color.name,
+        colors: colors,
+        images: images,
+      };
+      console.log("itemDetails", itemDetails);
+      return itemDetails;
     }
+    return null;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
 
 export default {
-    getItemDetails
+  getItemDetails,
 };
-
-
